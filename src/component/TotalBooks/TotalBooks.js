ff0616b6af7a5aec60b2032edd1aa87a2b9d18a1
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-05 01:39:54 +0600

    added fakedata to db

--- src/component/TotalBooks/TotalBooks.js
+++ src/component/TotalBooks/TotalBooks.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import FakeData from '../FakeData/FakeData.json'
+import fakeData from '../FakeData/FakeData.json'
 import Book from '../Book/Book'
 
 const TotalBooks = () => {
+
+    // adding fakeData to db
+    // const addAllBooks = () => {
+    //     fetch('http://localhost:5000/addBooks', {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify(fakeData)
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }
     return (
         <div className="row">
-            <button>Add Books</button>
+            {/* <button onClick={addAllBooks}>Add Books</button> */}
             {
-                FakeData.map(b => <Book book={b}></Book>)
+                fakeData.map(b => <Book book={b}></Book>)
             }
         </div>
     );
