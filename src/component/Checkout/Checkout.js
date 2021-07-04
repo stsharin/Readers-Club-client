c44747d020de380249dd3c55f569fb12f4b78ea0
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-05 03:21:26 +0600

    added checkout page design

--- src/component/Checkout/Checkout.js
+++ src/component/Checkout/Checkout.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Checkout = () => {
     return (
-        <div>
-            I am checkout bro
+        <div className="container col-md-6">
+            <div>
+                <h2 className="mt-5"><strong>Checkout</strong></h2>
+
+                <div className="shadow rounded mt-3 p-5">
+                    <table class="table table-borderless">
+                        <thead className="border-bottom text-secondary">
+                            <tr>
+                                <th scope="col">Description</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div> 
+            </div>
+            <button className="btn mt-3" style={{float: 'right', backgroundColor: '#6650e4', color: 'white' }}>Checkout</button>
         </div>
     );
 };
