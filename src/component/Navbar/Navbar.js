34f31e94d8ae5ac5323134ce785e8d819c6b9592
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-06 23:44:32 +0600

    icon

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand ms-5" href="/home">Book Shop</a>
+                    <a className="navbar-brand ms-5" href="/home">Readers Club</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
